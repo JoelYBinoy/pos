b3b5c8c3e51508844a8b622d16028c55f4d7e205
JoelYBinoy <joelyohannanbinoy@gmail.com>
2024-02-10 17:59:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import { Col, Row } from 'react-bootstrap';
 import './App.css';
+import Cart from './components/Cart';
+import SideBar from './components/SideBar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import AddToBillContextProvider from './context/addToBillContextProvider';
+import './assets/style1.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='mainDiv' >
+
+      <AddToBillContextProvider>
+
+    <Row>
+      <Col lg={1} md={1} xl={1} sm={1} className='sidebarapp_div' >
+        <SideBar/>
+      </Col>
+
+     
+
+      <Col  md={7} lg={7} xl={7} sm={7} className='appmid_div'  >
+      <Routes>
+        
+        <Route path='/' element={<Home/>} ></Route>
+        <Route path='search' element={<Search/>} ></Route>
+        
+      </Routes>
+      </Col>
+
+      
+
+      <Col  lg={4} md={3} xl={4} sm={4} className='appcart_div' >
+      <Cart/>
+      </Col>
+    </Row>
+
+    </AddToBillContextProvider>
+   
+    
     </div>
+    </>
   );
 }
 
